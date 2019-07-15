f9b08f18b05c23958bee42fc9dda343359eff08d
mozart25 <mozartlee28@gmail.com>
2019-07-16 01:25:18 +0900

    make button component for choosing methods

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.scss'
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div className="button-wrapper">
+        <label className="button-container"> 선택
+          <input type="checkbox"/>
+          <span className="checkmark"></span>
+        </label>
+      </div>
+    )
+  }
+}
+
+export default Button;
