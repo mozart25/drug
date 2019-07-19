ec6c05aa0f5a51468bd16462881979429a2fc122
mozart25 <mozartlee28@gmail.com>
2019-07-19 12:17:11 +0900

    solution search 페이지 제작을 위한 파일 생성

---
+++ src/Pages/Solution/Solution.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Solution.scss';
+
+class Solution extends React.Component {
+
+  render() {
+    return (
+      <div>
+        test page
+      </div>
+    );
+  }
+}
+
+export default Solution;
