e6e0a1b6d469b51b0d8efc370d02db57a40b5a66
mozart25 <mozartlee28@gmail.com>
2019-07-19 18:28:17 +0900

    ai algorithm 서칭 페이지 기본 틀 완료

---
+++ src/Pages/Solution/Sub.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Sub.scss';
+import SearchDrug from '../../Components/SearchDrug/SearchDrug';
+
+class Sub extends React.Component {
+
+  render() {
+    return (
+      <div className="sub-wrapper">
+        <main role="search">
+          <div>
+            <p className="sub-search-title">Drug AI algorithm rocommendation solution</p>
+            <SearchDrug
+            />
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Sub;
