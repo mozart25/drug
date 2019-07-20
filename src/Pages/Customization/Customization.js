dd37352b16888b63145b6d0e1f0eb5c7fd453293
mozart25 <mozartlee28@gmail.com>
2019-07-20 23:05:26 +0900

    customization page 기본 파일 생성 및 url 설정

---
+++ src/Pages/Customization/Customization.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Customization.scss';
+
+
+class Customization extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p> Customization test page </p>
+      </div>
+    );
+  }
+}
+
+export default Customization;
