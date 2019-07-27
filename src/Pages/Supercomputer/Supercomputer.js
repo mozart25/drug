c2e31c40c1a726d498cf26ae25352f4fdee97b73
mozart25 <mozartlee28@gmail.com>
2019-07-27 21:46:37 +0900

    supercomputer 반응형 페이지 작업을 위해 전체 UI 설계 재수정

--- src/Pages/Supercomputer/Supercomputer.js
+++ src/Pages/Supercomputer/Supercomputer.js
@@ -22,120 +22,27 @@ class Supercomputer extends React.Component {
 
   super();
 
-  this.updateChart = this.updateChart.bind(this);
-
   this.state = {
    gpuData:[],
-   curTime : null
+   currentGpuA : null,
+   currentGpuB : null,
   };
 }
   componentDidMount() {
     setInterval(this.updateChart, updateInterval);
 
-    setInterval( () => {
+    setInterval( ()=> {
       this.setState({
-    curTime : new Date().toLocaleString()
-  })
-},1000)
-  }
+        currentGpuA: Math.floor((Math.random() * (100-85+1)) + 85),
+        currentGpuB: Math.floor((Math.random() * (100-85+1)) + 85)
 
-  updateChart() {
-    var temp =[];
-    var dpsColor, dpsTotal = 0, deltaY, yVal;
-    var dps = this.chart.options.data[0].dataPoints;
-    var chart = this.chart;
-    for (var i = 0; i < dps.length; i++) {
-      deltaY = Math.round(2 + Math.random() *(-2-2));
-      yVal = deltaY + dps[i].y > 0 ? (deltaY + dps[i].y < 500 ? dps[i].y + deltaY : 500) : 0;
-      dpsColor = yVal >= 220 ? "#FD0E35" : yVal >= 210 ? "#FF6037" : yVal >= 200 ? "#00FEFE" : "#00e640";
-      dps[i] = {label: "GPU "+(i+1) , y: yVal, color: dpsColor};
-      dpsTotal += yVal;
-    }
+      })
 
-    chart.options.data[0].dataPoints = dps;
-    chart.options.title.text = `Total Usage ${dpsTotal}GB`
-
-    console.log("check dpsTotal", dpsTotal)
-    console.log("chart.options.data[0].dataPoints", chart.options.data[0].dataPoints)
-    console.log("dpsdpsdps", dps)
-    console.log("gpudatagpudata", this.state.gpuData)
-    chart.render()
-
+    }, 1000);
   }
 
 
   render() {
-
-    const percentage_a = 64;
-    const percentage_b = 53;
-    const percentage_c = 85;
-    const percentage_d = 77;
-
-    const efficiency_1 = ((Math.random() * (100-80+1)) + 80).toFixed(2);
-    const efficiency_2 = ((Math.random() * (100-80+1)) + 80).toFixed(2);
-    const efficiency_3 = ((Math.random() * (100-80+1)) + 80).toFixed(2);
-    const efficiency_4 = ((Math.random() * (100-80+1)) + 80).toFixed(2);
-
-    const temp_a1 = Math.floor((Math.random() * (100-85+1)) + 85);
-    const temp_a2 = Math.floor((Math.random() * (100-85+1)) + 85);
-    const temp_b1 = Math.floor((Math.random() * (100-85+1)) + 85);
-    const temp_b2 = Math.floor((Math.random() * (100-85+1)) + 85);
-    const temp_c1 = Math.floor((Math.random() * (100-85+1)) + 85);
-    const temp_c2 = Math.floor((Math.random() * (100-85+1)) + 85);
-    const temp_d1 = Math.floor((Math.random() * (100-85+1)) + 85);
-    const temp_d2 = Math.floor((Math.random() * (100-85+1)) + 85);
-
-
-
-    const Example = (props)=> {
-      return (
-          <div className="s-c-example">
-            <div className="gpu-items-wrapper">
-              <span id ="gpu-items-text"> {props.value}</span>
-              <span id ="gpu-items-digit"> {`${percentage_a}MB`} </span>
-
-            </div>
-            <p>{props.label}</p>
-              <div style={{ float:'left', width: 100 }}>{props.children}</div>
-              <div className="status-checking-box">
-                <div className="status-wrapper">
-                  <div className="status-box-used"></div> <span>Used </span>
-                </div>
-                <div className="status-wrapper">
-                  <div className="status-box-avail"></div> <span>Available </span>
-                </div>
-              </div>
-          </div>
-      );
-    }
-
-    const options = {
-      backgroundColor: null,
-      width: 310,
-      height:380,
-			theme: "light1",
-			title: {
-				text: "CPU Usage",
-        verticalAlign: "bottom",
-        horizontalAlign: "center",
-			},
-			axisY: {
-				suffix: "GB",
-        gridThickness: 0,
-			maximum: 500
-			},
-			data: [{
-				type: "column",
-				yValueFormatString: "#,###'GB'",
-				indexLabel: "{y}",
-				dataPoints: [
-					{ label: "GPU 1", y: 206 },
-					{ label: "GPU 2", y: 200 },
-					{ label: "GPU 3", y: 190 },
-					{ label: "GPU 4", y: 231 },
-				]
-			}]
-		}
 
     return (
       <div className='supercomputer-container'>
@@ -161,162 +68,14 @@ class Supercomputer extends React.Component {
         </div>
 
         <div className="s-usage">
-          <div className="s-gpu-wrapper">
-            <p className="s-gpu-service"> GPU </p>
-            <div className="gpu-status-wrapper">
-              <img src={require(`./gpu.png`)} alt=""/>
-              <div className="temp-wrapper">
-                <p style={{marginBottom: "20px", fontColor: "#fff"}}> GPU Average </p>
-                <p>{`GPU1 ${efficiency_1}%`}</p>
-                <p>{`GPU2 ${efficiency_2}%`}</p>
-                <p>{`GPU3 ${efficiency_3}%`}</p>
-                <p>{`GPU4 ${efficiency_4}%`}</p>
-             </div>
-            </div>
-            <div className="gpu-battery-wrapper">
-              <Battery
-              id="GPU1"
-              randValue={efficiency_1}
-              />
-              <Battery2
-              id="GPU2"
-              randValue={efficiency_2}
-              />
-              <Battery3
-              id="GPU3"
-              randValue={efficiency_3}
-              />
-              <Battery4
-              id="GPU4"
-              randValue={efficiency_4}
-              />
-            </div>
-          </div>
-
-          <img src={require(`./mac.png`)} alt=""/>
-          <div className="s-memory-wrapper">
-            <p className="s-m-service"> Memory </p>
-            <div className="s-m-progress-bar">
-              <Example value="GPU1">
-                <AnimatedProgressProvider
-                  valueStart={temp_a1}
-                  valueEnd={temp_a2}
-                  duration={0.7}
-                  easingFunction={easeQuadInOut}
-                  repeat
-                >
-                  {value => {
-                    const roundedValue = Math.round(value);
-                    return (
-                      <CircularProgressbar
-                        value={value}
-                        text={`${roundedValue}%`}
-                        styles={buildStyles(
-                          {
-                            pathTransition: "none",
-                            pathColor: `rgba(0, 230, 64, 1)`,
-                            textColor: `rgba(0, 230, 64, 1)`
-                          }
-                        )
-                      }
-                      />
-                    );
-                  }}
-                </AnimatedProgressProvider>
-              </Example>
-              <Example value="GPU2">
-                <AnimatedProgressProvider
-                  valueStart={temp_b1}
-                  valueEnd={temp_b2}
-                  duration={0.7}
-                  easingFunction={easeQuadInOut}
-                  repeat
-                >
-                  {value => {
-                    const roundedValue = Math.round(value);
-                    return (
-                      <CircularProgressbar
-                        value={value}
-                        text={`${roundedValue}%`}
-                        styles={buildStyles(
-                          {
-                            pathTransition: "none",
-                            pathColor: `rgba(0, 230, 64, 1)`,
-                            textColor: `rgba(0, 230, 64, 1)`
-                          }
-                        )
-                      }
-                      />
-                    );
-                  }}
-                </AnimatedProgressProvider>
-              </Example>
-              <Example value="GPU3">
-                <AnimatedProgressProvider
-                  valueStart={temp_c1}
-                  valueEnd={temp_c2}
-                  duration={0.7}
-                  easingFunction={easeQuadInOut}
-                  repeat
-                >
-                  {value => {
-                    const roundedValue = Math.round(value);
-                    return (
-                      <CircularProgressbar
-                        value={value}
-                        text={`${roundedValue}%`}
-                        styles={buildStyles(
-                          {
-                            pathTransition: "none",
-                            pathColor: `rgba(0, 230, 64, 1)`,
-                            textColor: `rgba(0, 230, 64, 1)`
-                          }
-                        )
-                      }
-                      />
-                    );
-                  }}
-                </AnimatedProgressProvider>
-              </Example>
-              <Example value="GPU4">
-                <AnimatedProgressProvider
-                  valueStart={temp_d1}
-                  valueEnd={temp_d2}
-                  duration={0.7}
-                  easingFunction={easeQuadInOut}
-                  repeat
-                >
-                  {value => {
-                    const roundedValue = Math.round(value);
-                    return (
-                      <CircularProgressbar
-                        value={value}
-                        text={`${roundedValue}%`}
-                        styles={buildStyles(
-                          {
-                            pathTransition: "none",
-                            pathColor: `rgba(0, 230, 64, 1)`,
-                            textColor: `rgba(0, 230, 64, 1)`
-                          }
-                        )
-                      }
-                      />
-                    );
-                  }}
-                </AnimatedProgressProvider>
-              </Example>
-            </div>
-          </div>
-          <div className="s-usage-wrapper">
-            <p className="s-m-service"> Usage </p>
-            <CanvasJSChart options = {options}
-  					 onRef={ref => this.chart = ref}
-            />
-            <div className="s-m-u-status-wapper">
-                <Clock style={{fontFamily: 'Oswald',color:'#FF8C00'}}format={'YYYY/MM/DD HH:mm:ss'} ticking={true} timezone={'Asia/Seoul'} />
-            </div>
+          <img className="final-mac-img" src={require(`./mac.png`)} alt=""/>
+            <div className="gpu-total-wrapper">
+              <div>gpu </div>
+              <div>memory </div>
+              <div>usage </div>
           </div>
         </div>
+
 
         <div className="s-footer">
           <div className="s-footer-top">
