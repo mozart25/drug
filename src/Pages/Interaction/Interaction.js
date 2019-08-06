b871fb18848eb8beb00febc6a7b69ce25a435729
mozart25 <mozartlee28@gmail.com>
2019-08-07 01:40:08 +0900

    개별 drug 페이지 배경 및 색 변경

--- src/Pages/Interaction/Interaction.js
+++ src/Pages/Interaction/Interaction.js
@@ -18,9 +18,9 @@ class Interaction extends React.Component {
   render() {
     return (
       <div className="search-wrapper">
-      <Helmet>
-        <title>Drug Data Integration service-BioAI</title>
-      </Helmet>
+        <Helmet>
+          <title>Drug Data Integration service-BioAI</title>
+        </Helmet>
         <Header />
         <main role="search" className="main-wrapper">
           <div className="main-comment">
@@ -35,11 +35,23 @@ class Interaction extends React.Component {
         </main>
         <div className="main-bg iframe-wrapper">
           <div className="c-card iframe-1st-compo">
-            <iframe style={{border:'0.5px solid lightgray'}} src="http://localhost:3000/acetaminophen" height="100%" width="100%"></iframe>
+            <iframe className="frame-customizing" style={{border:'0.5px solid lightgray'}} src="http://localhost:3000/acetaminophen" height="100%" width="100%"></iframe>
           </div>
+          <div className="iframe-compo-divide"> </div>
           <div className="c-card iframe-2nd-compo">
-            <iframe style={{border:'0.5px solid lightgray'}} src="http://localhost:3000/escitalopram" height="100%" width="100%"></iframe>
+            <iframe className="frame-customizing" style={{border:'0.5px solid lightgray'}} src="http://localhost:3000/escitalopram" height="100%" width="100%"></iframe>
           </div>
+        </div>
+        <div>
+          <div className="divide-line"> </div>
+        </div>
+        <h2> Acetaminophen & Escitalopram Interaction </h2>
+        <p> The metabolism of Escitalopram can be increased when combined with Acetaminophen.</p>
+        <div>
+          <div className="divide-line"> </div>
+        </div>
+        <div>
+          <h2> Preliminary Research </h2>
         </div>
       </div>
     );
