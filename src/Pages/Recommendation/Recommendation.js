8fdfe1f8479228af33608ad262499de1f9223c2a
mozart25 <mozartlee28@gmail.com>
2019-08-06 21:08:16 +0900

    recommendaion page header logo 삽입

--- src/Pages/Recommendation/Recommendation.js
+++ src/Pages/Recommendation/Recommendation.js
@@ -303,35 +303,10 @@ class Recommendation extends React.Component {
 
     return (
       <div className="recommend-container">
-      <Helmet>
-        <title>Drug AI algorithm recommendation solution-BioAI</title>
-      </Helmet>
-      <Header />
-        <nav className="navbar-recommendation navbar-expand-lg navbar-light bg-white header">
-          <div className="header-logo">
-            <a className="navbar-brand" href="/">
-                <p> BioAI</p>
-            </a>
-          </div>
-          <div className="collapse navbar-collapse order-1" id="navbarSupportedContent">
-            <div className="order-3">
-              <div className="navbar-nav">
-                <ul className="navbar-nav ml-auto navbar-subscribe">
-                  <li className="nav-item nav-link-right">
-                    <a className="nav-link-right nav-textual-link" href="https://twitter.com/paperswithcode">
-                      About</a>
-                  </li>
-                  <li className="nav-item nav-link-right">
-                    <a className="nav-link-right nav-textual-link" href="https://join.slack.com/t/paperswithcode/shared_invite/enQtNTgyMTUxODUwNDY2LTQzZWQ1ZjE4NmRhZjA4NDY1ZDcyMjdmOGZlOGJjM2VkOGNkYjczYmRhOGU5ZGZkNWQ4OGE3MzJjZjBjZTgwNzA">
-                      Blog</a>
-                  </li>
-                  <li className="nav-item nav-link-right"><a className="nav-link-right nav-textual-link" style={{position: 'relative', top: '1px'}} href="/about">Contact us</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </nav>
-
+        <Helmet>
+          <title>Drug AI algorithm recommendation solution-BioAI</title>
+        </Helmet>
+        <Header />
           <div className="container">
             <div className="container content content-buffer">
               <div className="title">
