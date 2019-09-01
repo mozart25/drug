fb59d03135b12f179ce4c4e248ec22a870f91ffb
mozart25 <mozartlee28@gmail.com>
2019-09-01 21:01:19 +0900

    final link url path setting with dropdown header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,13 +5,25 @@ class Header extends React.Component {
   render() {
     return (
       <header role="banner" className="NavMain-StyledNavbar-module--cls1--3GekH navbar navbar-expand navbar-dark">
-        <img className="header-icon" src="https://druginteraction.s3.ap-northeast-2.amazonaws.com/bioai_logo.png" style={{width: "67px"}}alt=""/>
+        <a href="http://52.79.200.208/">
+          <img className="header-icon" src="https://druginteraction.s3.ap-northeast-2.amazonaws.com/bioai_logo.png" style={{width: "67px"}} alt=""/>
+        </a>
         <div className="d-none d-md-flex navbar-nav header-menu" role="navigation" id="top">
-          <a href="/" data-rb-event-key="/" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">About</a>
-          <a href="/getting-started/introduction" data-rb-event-key="/getting-started/introduction" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">Research</a>
-          <a href="/components/alerts" data-rb-event-key="/components/alerts" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">Services</a>
-          <a href="/components/alerts" data-rb-event-key="/components/alerts" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">Team</a>
-          <a href="/components/alerts" data-rb-event-key="/components/alerts" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">Contact</a>
+          <a href="http://52.79.200.208/" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">About</a>
+          <a href="http://52.79.200.208/" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">Research</a>
+          <div className="dropdown show">
+            <a className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Services
+            </a>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a className="dropdown-item" href="http://52.79.200.208/services/integration"> Drug Data Integration Service </a>
+              <a className="dropdown-item" href="http://52.79.200.208/services/recommendation"> Drug AI Algorithm Recommendation Solution </a>
+              <a className="dropdown-item" href="http://52.79.200.208/services/estimates"> Estimate your cost on Customized AI Study </a>
+              <a className="dropdown-item" href="http://52.79.200.208/services/supercomputer"> Easy Supercomputer Use Service </a>
+            </div>
+          </div>
+          <a href="http://52.79.200.208/" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">Team</a>
+          <a href="http://52.79.200.208/" className="NavMain-StyledNavLink-module--cls1--3NJV5 nav-link">Contact</a>
         </div>
      </header>
     )
--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -28,7 +28,7 @@ class Search extends React.Component {
               {`Drug-Drug Interaction Integrated Database`}
             </p>
           </div>
-          <div id="search-drug-wrapper" style={{display:"flex", marginTop: "3%"}}>
+          <div id="search-drug-wrapper" style={{display:"flex", marginTop: "5%"}}>
             <SearchDrug
             />
           </div>
